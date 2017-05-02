dceacfc8a32fe7d333b3aa8e09be2b35166e5dc1
Sultanxda <sultanxda@gmail.com>
2017-05-02 09:58:11 -0700

    mmc: queue: Configure the I/O sched switcher for mmc block devices
    Change the I/O scheduler to noop when the screen turns off for mmc block
    devices.
    
    Change-Id: I5db2f58ccd8ceb9333c3caf529dddcd8274d0366
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/mmc/card/queue.c
+++ drivers/mmc/card/queue.c
@@ -17,6 +17,7 @@
 #include <linux/scatterlist.h>
 #include <linux/bitops.h>
 #include <linux/vmalloc.h>
+#include <linux/iosched_switcher.h>
 
 #include <linux/mmc/card.h>
 #include <linux/mmc/host.h>
@@ -419,6 +420,8 @@ success:
 		ret = PTR_ERR(mq->thread);
 		goto free_bounce_sg;
 	}
+
+	init_iosched_switcher(mq->queue);
 
 	return 0;
  free_bounce_sg:
